fba46f08738b02881aca50cbde43205ea0e057b3
Elgountari-Ayoub <elgountariayoub22@gmail.com>
2023-09-25 17:47:48 +0100

    update maCNSSdb database

--- maCNSSdb.sql
+++ maCNSSdb.sql
@@ -1,6 +1,5 @@
 -- Database: maCNSSdb
 
--- DROP DATABASE IF EXISTS "maCNSSdb";
 
 CREATE DATABASE "maCNSSdb"
     WITH
@@ -14,7 +13,7 @@ CREATE DATABASE "maCNSSdb"
 	
 -- Create the agentCnss table
 CREATE TABLE agentCnss (
-    cnss SERIAL PRIMARY KEY,
+    cnss varchar(255) PRIMARY KEY,
     nom VARCHAR(255),
     prenom VARCHAR(255),
     ville VARCHAR(255),
@@ -25,8 +24,8 @@ CREATE TABLE agentCnss (
 );
 -- Create the patient table
 CREATE TABLE patient (
-    immatricule SERIAL PRIMARY KEY,
-    cnss INT,
+    immatricule varchar(255) PRIMARY KEY,
+    cnss varchar(255),
     nom VARCHAR(255),
     prenom VARCHAR(255),
     ville VARCHAR(255),
@@ -37,24 +36,94 @@ CREATE TABLE patient (
     
 );
 
+-- Create the admin table
+CREATE TABLE admin (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255),
+    pass VARCHAR(255)
+);
+
 -- Create the dossier table
 CREATE TABLE dossier (
     matricule int PRIMARY KEY,
     etat VARCHAR(50),
-    agentCnss_cnss INT,
-    patient_immatricule INT,
+    agentCnss_cnss VARCHAR(255),
+    patient_immatricule varchar(255),
     FOREIGN KEY (agentCnss_cnss) REFERENCES agentCnss (cnss),
     FOREIGN KEY (patient_immatricule) REFERENCES patient (immatricule)
 );
 
--- Create the document table
-CREATE TABLE document (
+-- Create the medcine table
+CREATE TABLE medcine (
+    INPE VARCHAR(50) PRIMARY KEY,
+    address VARCHAR(255),
+	nom VARCHAR(255),
+	prenom VARCHAR(255),
+	type VARCHAR(255)
+);
+
+-- Create the organism table
+CREATE TABLE laboratoire (
+    INPE VARCHAR(50) PRIMARY KEY,
+    address VARCHAR(255),
+	label varchar(255) 
+);
+
+-- Create the organism table
+CREATE TABLE radiologie (
+    INPE VARCHAR(50) PRIMARY KEY,
+    address VARCHAR(255),
+	label varchar(255) 
+);
+
+-- Create the Radio table
+CREATE TABLE radio (
+    id int PRIMARY KEY,
+	radiologie_INPE varchar(255),
+    prix DECIMAL(10, 2),
+    taux DECIMAL(5, 2),
+    description TEXT,
+    dossier_matricule INT,
+	type varchar(255),
+    FOREIGN KEY (dossier_matricule) REFERENCES dossier (matricule),
+    FOREIGN KEY (radiologie_INPE) REFERENCES radiologie (INPE)
+);
+
+-- Create the Scanner table
+CREATE TABLE scanner (
+    id int PRIMARY KEY,
+	radiologie_INPE varchar(255),
+    prix DECIMAL(10, 2),
+    taux DECIMAL(5, 2),
+    description TEXT,
+    dossier_matricule INT,
+    FOREIGN KEY (dossier_matricule) REFERENCES dossier (matricule),
+	FOREIGN KEY (radiologie_INPE) REFERENCES radiologie (INPE)
+);
+
+-- Create the analyse table
+CREATE TABLE analysee (
+    id int PRIMARY KEY,
+	laboratoire_INPE varchar(255),
+    prix DECIMAL(10, 2),
+    taux DECIMAL(5, 2),
+    description TEXT,
+    dossier_matricule INT,
+	type varchar(255),
+    FOREIGN KEY (dossier_matricule) REFERENCES dossier (matricule),
+	FOREIGN KEY (laboratoire_INPE) REFERENCES laboratoire (INPE)
+);
+
+-- Create the ordonnance table
+CREATE TABLE ordonnance (
     id int PRIMARY KEY,
     prix DECIMAL(10, 2),
     taux DECIMAL(5, 2),
     description TEXT,
     dossier_matricule INT,
-    FOREIGN KEY (dossier_matricule) REFERENCES dossier (matricule)
+	medcine_INPE varchar(255),
+    FOREIGN KEY (dossier_matricule) REFERENCES dossier (matricule),
+    FOREIGN KEY (medcine_INPE) REFERENCES medcine (INPE)
 );
 
 -- Create the categorie table
@@ -75,17 +144,4 @@ CREATE TABLE medicament (
     FOREIGN KEY (categorie_id) REFERENCES categorie (id)
 );
 
--- Create the laboratoire table
-CREATE TABLE laboratoire (
-    id SERIAL PRIMARY KEY,
-    label VARCHAR(255)
-);
 
--- Create the medcine table
-CREATE TABLE medcine (
-    INPE VARCHAR(50) PRIMARY KEY,
-    address VARCHAR(255),
-	nom VARCHAR(255),
-	prenom VARCHAR(255),
-	type VARCHAR(255)
-);
